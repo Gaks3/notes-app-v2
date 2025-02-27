2702bf88d01a0e1fba834373d876084308320b4f
Gaks3 <dwi66116@gmail.com>
2025-02-27 19:30:06 +0700

    feat: add loading-spinner, tab-navigation, and delete-dialog component

--- src/script/components/index.js
+++ src/script/components/index.js
@@ -1,6 +1,7 @@
 import "./app-bar.js";
-
 import "./note-item.js";
 import "./note-form.js";
-
 import "./app-footer.js";
+import "./loading-spinner.js";
+import "./tab-navigation.js";
+import "./delete-dialog.js";
