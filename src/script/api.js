b9b8b125b312683284f82d32efa1fcb19da1d215
Gaks3 <dwi66116@gmail.com>
2025-02-27 19:26:45 +0700

    feat: add get and post archived notes

--- src/script/api.js
+++ src/script/api.js
@@ -3,7 +3,12 @@ const BASE_URL = "https://notes-api.dicoding.dev/v2";
 export const getNotes = async () => {
   const response = await fetch(`${BASE_URL}/notes`);
   const data = await response.json();
+  return data.data;
+};
 
+export const getArchivedNotes = async () => {
+  const response = await fetch(`${BASE_URL}/notes/archived`);
+  const data = await response.json();
   return data.data;
 };
 
@@ -19,4 +24,20 @@ export const addNote = async (title, body) => {
 
 export const deleteNote = async (id) => {
   await fetch(`${BASE_URL}/notes/${id}`, { method: "DELETE" });
+};
+
+export const archiveNote = async (id) => {
+  const response = await fetch(`${BASE_URL}/notes/${id}/archive`, {
+    method: "POST",
+  });
+
+  return response.json();
+};
+
+export const unarchiveNote = async (id) => {
+  const response = await fetch(`${BASE_URL}/notes/${id}/unarchive`, {
+    method: "POST",
+  });
+
+  return response.json();
 };
